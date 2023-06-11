f01dacb0d4bb0aa46fd20a31b73a9339d13e2f85
Renato Oliveira <renatorroliveira@gmail.com>
2023-06-10 19:55:57 -0700

    Switching to NestJS

--- app/src/app.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import Koa from "koa";
-
-const serverPort = process.env.APP_PORT;
-const app = new Koa();
-
-app.listen(serverPort);
-console.log(`Server running at port ${serverPort}...`);
